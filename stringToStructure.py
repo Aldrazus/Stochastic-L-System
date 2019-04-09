87e40ce7c69847abb54de161e5983e65e2cf0a33
Alex Aquino <aaquino1223@gmail.com>
2019-04-08 23:20:25 -0400

    Tweaks to current python file
    Created visual studio project

--- stringToStructure.py
+++ stringToStructure.py
@@ -1,6 +1,26 @@
 import wselect
+import random
+import numpy
 from pathlib import Path
 
+# blocks number and size
+blocks = {'1':[0.84,0.84], '2':[0.85,0.43], '3':[0.43,0.85], '4':[0.43,0.43],
+          '5':[0.22,0.22], '6':[0.43,0.22], '7':[0.22,0.43], '8':[0.85,0.22],
+          '9':[0.22,0.85], '10':[1.68,0.22], '11':[0.22,1.68],
+          '12':[2.06,0.22], '13':[0.22,2.06]}
+
+# blocks number and name
+# (blocks 3, 7, 9, 11 and 13) are their respective block names rotated 90 derees clockwise
+block_names = {'1':"SquareHole", '2':"RectFat", '3':"RectFat", '4':"SquareSmall",
+               '5':"SquareTiny", '6':"RectTiny", '7':"RectTiny", '8':"RectSmall",
+               '9':"RectSmall",'10':"RectMedium",'11':"RectMedium",
+               '12':"RectBig",'13':"RectBig"}
+
+# defines the levels area (ie. space within which structures/platforms can be placed)
+level_width_min = -3.0
+level_width_max = 9.0
+level_height_min = -3.5         # only used by platforms, ground structures use absolute_ground to determine their lowest point
+level_height_max = 6.0
 
 class LSystem:
 
@@ -27,14 +47,17 @@ searches and replaces symbols w, s, i, a with wood, stone, ice, air respectively
 def stringToStructure(lsystem, filename):
     materialKey = {'W': "wood", 'S':"stone", 'I':"ice", 'A':"air"}
     windowRange = [-4, 4]
+    xPos = random.uniform(-4, 4)
+    yPos = -3.5 + len(lsystem.iterations) * 0.22
     f = open(filename, 'w')
     startFile(f)
     for axiom in range(0, len(lsystem.iterations)):
+        yPos = yPos - axiom * 0.22
         for symbol in range(0, len(lsystem[axiom])):
             if materialKey[lsystem[axiom][symbol]] is not "air":
                 f.write(xmlify("SquareTiny", materialKey[lsystem[axiom][symbol]],
-                               (float(symbol) / len(lsystem[axiom]))*(windowRange[1] - windowRange[0]) + windowRange[0],
-                               (float(symbol) / len(lsystem[axiom]))*(windowRange[1] - windowRange[0]) + windowRange[0],
+                               xPos - ((len(lsystem[axiom]) / 2) - symbol) * 0.22,
+                               yPos,
                                0))
     endFile(f)
     f.close()
